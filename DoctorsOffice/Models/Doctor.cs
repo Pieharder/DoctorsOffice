0ca22c4e129ab95c1389abfb33f02334acc35f67
Dusty <dustymccord@gmail.com>
2020-03-23 15:00:41 -0700

    Add Specialty migration and DoctorSpecialty migration.
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- DoctorsOffice/Models/Doctor.cs
+++ DoctorsOffice/Models/Doctor.cs
@@ -7,13 +7,13 @@ namespace DoctorsOffice.Models
     public Doctor()
     {
       this.Patients = new HashSet<DoctorPatient>();
+      this.Specialty = new HashSet<DoctorSpecialty>();
     }
     public int DoctorId { get; set; }
     public string Name { get; set; }
 
-    public string Specialty { get; set; }
-
     public virtual ICollection<DoctorPatient> Patients { get; set; }
+    public virtual ICollection<DoctorSpecialty> Specialty { get; set; }
   }
 
 }
